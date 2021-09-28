22c2a13e58aa7fc5d1efb16ee7205e1173c5fd1f
Ryely <ryely.moore@outlook.com>
2021-09-28 03:33:03 -0600

    updated to allow the service worker to direct the application when it is offline and tested in browser

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,3 +17,38 @@ self.addEventListener('install', function (event) {
         })
     )
 })
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function(key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );    
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log('fetch request : ' + event.request.url)
+    event.respondWith(
+        caches.match(event.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + event.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + event.request.url)
+                return fetch(event.request)
+            }
+        })
+    )
+})
